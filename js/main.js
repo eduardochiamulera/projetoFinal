0159ce543cb3c61b10598ff22b72230cdd809be5
Eduardo Chiamulera <eduardo.chiamulera@totvs.com.br>
2020-07-08 14:18:02 -0300

    crud produtos completo

--- js/main.js
+++ js/main.js
@@ -14,7 +14,15 @@ var app = new Vue({
             quantidade: "", 
             loja: {
                 id : "",
-                nome: ""
+                nome: "",
+                telefone: "",
+                endereco: "",
+                cep: "",
+                bairro: "",
+                cidade: "",
+                estado: "",
+                complemento: "",
+                numero: "",
              }
         },
         currentProduct: {}
@@ -37,16 +45,26 @@ var app = new Vue({
             })
         },
         addProduto(novoProduto){
-            console.log("addProduto" + novoProduto.loja.id);
+            const self = this;
+            console.log("addProduto " + novoProduto.loja.id);
             axios.post("http://localhost:8000/api/produtos",novoProduto)
             .then(function(response){
                 if(response.data.error){
                     app.errorMsg = response.data.message;
                 }else{
-                    app.produtos = response.data;
+                    self.novoProduto = {
+                        nome: "", 
+                        preco: "", 
+                        quantidade: "", 
+                        loja: {
+                            id : "",
+                            nome: ""
+                        }}
+                    app.successMsg = "Produto Cadastrado com sucesso";
+                    self.getAllProdutos();
                 }
             }).catch(function(error){
-                app
+                app.errorMsg = error;
             })
         },
         getAllLojas(){
@@ -62,8 +80,60 @@ var app = new Vue({
                 app.errorMsg = error.data;
             })
         },
+        putProdutos(novoProduto){
+            const self = this;
+            axios.put("http://localhost:8000/api/produtos/"+novoProduto.id,novoProduto)
+            .then(function(response){
+                if(response.data.error){
+                    app.errorMsg = response.data.message;
+                }else{
+                    self.novoProduto = {
+                        nome: "", 
+                        preco: "", 
+                        quantidade: "", 
+                        loja: {
+                            id : "",
+                            nome: ""
+                        }}
+                    app.successMsg = "Produto Atualizado com sucesso";
+                    self.getAllProdutos();
+                }
+            }).catch(function(error){
+                app.errorMsg = error.data;
+            })
+        },
+        deleteProdutos(novoProduto){
+            const self = this;
+            axios.delete("http://localhost:8000/api/produtos/"+novoProduto.id)
+            .then(function(response){
+                if(response.data.error){
+                    app.errorMsg = response.data.message;
+                }else{
+                    self.novoProduto = {
+                        nome: "", 
+                        preco: "", 
+                        quantidade: "", 
+                        loja: {
+                            id : "",
+                            nome: ""
+                        }}
+                    app.successMsg = "Produto Deletado com sucesso";                    
+                    self.getAllProdutos();
+                }
+            }).catch(function(error){
+                app.errorMsg = error.data;
+            })
+        },
         salvar(){
+            this.novoProduto.loja.id = this.novoProduto.loja.id.replace("Loja","");
             this.addProduto(this.novoProduto);
+            console.log("salvar " + this.novoProduto)
+        },
+        atualizar(produto){
+            this.novoProduto = produto;
+        },
+        deletar(produto){
+            this.novoProduto = produto;
         }
     }
 })
